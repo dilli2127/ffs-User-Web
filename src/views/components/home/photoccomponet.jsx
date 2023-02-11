8d8ef3d29c79157556edfb8c12320d96a21d85bb
dilli2127 <dilli4904@gmail.com>
2023-02-11 16:16:03 +0530

    fixing

--- src/views/components/home/photoccomponet.jsx
+++ src/views/components/home/photoccomponet.jsx
@@ -16,7 +16,7 @@ const LandingPage = () => {
           <Box height="500px" backgroundColor="#fb6d0cd9" width="100%">
           <HStack>
             <Box width="50%" backgroundColor="amber.600" height="450px" marginTop="25px" borderRightRadius="30px">
-
+ddddddddddddddddddddddddddddddd
             </Box>
           </HStack>
           </Box>
