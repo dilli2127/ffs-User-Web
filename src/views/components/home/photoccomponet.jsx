e8c6603262b080f23bea5a171b270ad716002a00
dilli2127 <dilli4904@gmail.com>
2023-02-11 21:20:32 +0530

    ui

--- src/views/components/home/photoccomponet.jsx
+++ src/views/components/home/photoccomponet.jsx
@@ -13,13 +13,23 @@ const LandingPage = () => {
   return (
     <>
       <VStack>
-          <Box height="500px" backgroundColor="#fb6d0cd9" width="100%">
+        <Box height="500px" backgroundColor="#fb6d0cd9" width="100%">
           <HStack>
-            <Box width="50%" backgroundColor="amber.600" height="450px" marginTop="25px" borderRightRadius="30px">
-ddddddddddddddddddddddddddddddd
-            </Box>
+            <Box
+              marginTop="25px"
+              borderRightRadius="30px"
+              style={{
+                backgroundImage:
+                  'url("https://pub-c9841409a5664691accafda9ed7f1b86.r2.dev/062A9143.jpg")',
+                height: 450,
+                width: "50%",
+                justifyContent: "center",
+                alignItems: "center",
+                objecetFit:"cover"
+              }}
+            ></Box>
           </HStack>
-          </Box>
+        </Box>
       </VStack>
     </>
   );
